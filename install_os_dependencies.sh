dd0b4ac872243476caf527ff621e8837ce8cd023
Luciano Afranllie Ruiz <luafran@gmail.com>
2015-09-12 21:01:00 -0300

    Remove python2.7 and python3 from reqs

--- install_os_dependencies.sh
+++ install_os_dependencies.sh
@@ -6,8 +6,6 @@ apt-get -y install python-pip
 apt-get -y install python-distribute
 apt-get -y install python-tox
 apt-get -y install python-dev
-apt-get -y install python2.7-dev
-apt-get -y install python3-dev
 apt-get -y install libcurl4-openssl-dev
 apt-get -y install libev4 libev-dev
 
