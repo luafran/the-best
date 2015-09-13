43255313abe241e8191065061038b749ab9b7631
Luciano Afranllie Ruiz <luafran@gmail.com>
2015-09-12 21:01:33 -0300

    Add mongo file to clean_env.sh

--- clean_env.sh
+++ clean_env.sh
@@ -3,4 +3,4 @@ rm -rf coverage-report/
 rm -rf pylint-report/
 rm -f .coverage
 rm -f nosetests.xml
-rm -rf test/db/local* test/db/mongo* test/db/journal
+rm -rf test/db/local* test/db/mongo* test/db/journal test/db/storage.bson
