6ea0fcfebe4964a340c56b636a5f490799e74c54
Luciano Afranllie <luafran@gmail.com>
2015-10-18 21:19:24 -0300

    Remove unused methods

--- thebest/app/api.py
+++ thebest/app/api.py
@@ -79,31 +79,3 @@ class Application(object):
         # ToDo: convert external item to internal item
         result = yield items_repository.add_answer(question, answer)
         raise gen.Return(result)
-
-
-@gen.coroutine
-def get_items_q_a(question, answer):
-    hits = yield items_repository.get_items_q_a(question.lower(), answer.lower())
-    hits = hits.get(items_repository.HITS_TAG)
-    result = [hit.get('_source') for hit in hits]
-
-    raise gen.Return(result)
-
-
-@gen.coroutine
-def get_items_q(question):
-    result_list = []
-    hits = yield items_repository.get_items_q(question.lower())
-    hits = hits.get('hits')
-
-    for hit in hits:
-        source = hit.get(items_repository.SOURCE_TAG)
-        result_item = {
-            ID_TAG: hit.get(items_repository.ID_TAG),
-            QUESTION_TAG: source.get(items_repository.QUESTION_TAG),
-            ANSWER_TAG: source.get(items_repository.ANSWER_TAG)
-        }
-
-        result_list.append(result_item)
-
-    raise gen.Return(result_list)
