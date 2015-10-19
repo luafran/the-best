fc8ef14a9a1b4dd41feb4fe7777743fc4392a42b
rorobled <ra.l.o.robledo@gmail.com>
2015-10-19 16:07:50 -0300

    completing the items_repository migration

--- thebest/app/api.py
+++ thebest/app/api.py
@@ -26,12 +26,7 @@ class Application(object):
         items = yield self.items_repository.get_question_suggestions(text)
 
         suggestions = []
-        for item in items:
-            suggestion = {
-
-                TEXT_TAG: item
-            }
-
+        for suggestion in items:
             suggestions.append(suggestion)
 
         raise gen.Return(suggestions)
@@ -45,29 +40,25 @@ class Application(object):
         items = yield self.items_repository.get_answer_suggestions(question, text)
 
         suggestions = []
-        for item in items:
-            suggestion = {
-                TEXT_TAG: item
-            }
-
+        for suggestion in items:
             suggestions.append(suggestion)
 
         raise gen.Return(suggestions)
 
     @gen.coroutine
     def get_best_answer(self, question):
-        items = yield items_repository.get_best_answers(question)
+        items = yield self.items_repository.get_best_answers(question)
 
         raise gen.Return(items[0])
 
     @gen.coroutine
     def add_question(self, question):
-        yield items_repository.add_question(question)
+        yield self.items_repository.add_question(question)
         raise gen.Return(None)
 
     @gen.coroutine
     def get_system_question(self):
-        items = yield items_repository.get_system_questions()
+        items = yield self.items_repository.get_system_questions()
         total = len(items)
         rand_index = randrange(total-1)
         item = items[rand_index] if items else None
@@ -77,5 +68,5 @@ class Application(object):
     @gen.coroutine
     def process_user_answer(self, question, answer):
         # ToDo: convert external item to internal item
-        result = yield items_repository.add_answer(question, answer)
+        result = yield self.items_repository.add_answer(question, answer)
         raise gen.Return(result)
