967796473cfa5f0b4d88faf668808280cac1bb3f
Luciano Afranllie <luafran@gmail.com>
2015-10-24 15:50:05 -0300

    Use hash of current commit as service version

--- ci/build_packages.sh
+++ ci/build_packages.sh
@@ -1,9 +1,16 @@
-build_number=$2
 root_dir=$1
+build_number=$2
 dist_dir=${root_dir}/dist
+
+commit_id=$(git rev-parse HEAD)
+echo $commit_id
+
+sed -i"" 's/1.0.0/'$commit_id'/g' setup.py
 
 python setup.py egg_info --tag-build ".${build_number}" sdist --dist-dir ${dist_dir}
 if [ $? -ne 0 ]; then
     echo "Error: failed to generate pip file"
     exit 1
 fi
+
+git checkout -- setup.py
