8d50f23058d1b42b60cb6e65f2daecf2134b6dd5
Luciano Afranllie <luafran@gmail.com>
2015-10-27 20:09:40 -0300

    Add v1 to api urls

--- thebest/handlers/best_answer.py
+++ thebest/handlers/best_answer.py
@@ -20,25 +20,6 @@ class BestAnswerHandlerV1(base.BaseHandler):
         else:
             app = api.Application(self.context, self.application_settings.items_repository)
             items = yield app.get_best_answers(question, limit)
-            response = items[0] if items else None
-
-        self.build_response(response)
-
-
-class BestAnswerHandlerV2(base.BaseHandler):
-
-    @decorators.api_key_authorization
-    @gen.coroutine
-    def get(self):
-
-        limit = int(self.get_query_argument(api.LIMIT_TAG, '1'))
-
-        question = self.get_query_argument(api.QUESTION_TAG, None)
-        if not question:
-            response = exceptions.MissingArgumentValue('Missing argument {0}'.format(api.QUESTION_TAG))
-        else:
-            app = api.Application(self.context, self.application_settings.items_repository)
-            items = yield app.get_best_answers(question, limit)
             response = {'answers': items[0:limit] if items else []}
 
         self.build_response(response)
--- thebest/handlers/system_question.py
+++ thebest/handlers/system_question.py
@@ -20,25 +20,6 @@ class SystemQuestionHandlerV1(base.BaseHandler):
         else:
             app = api.Application(self.context, self.application_settings.items_repository)
             items = yield app.get_system_questions(question, limit)
-            response = items[0] if items else None
-
-        self.build_response(response)
-
-
-class SystemQuestionHandlerV2(base.BaseHandler):
-
-    @decorators.api_key_authorization
-    @gen.coroutine
-    def get(self):
-
-        limit = int(self.get_query_argument(api.LIMIT_TAG, '1'))
-
-        question = self.get_query_argument(api.QUESTION_TAG, None)
-        if not question:
-            response = exceptions.MissingArgumentValue('Missing argument {0}'.format(api.QUESTION_TAG))
-        else:
-            app = api.Application(self.context, self.application_settings.items_repository)
-            items = yield app.get_system_questions(question, limit)
             response = {'questions': items[0:limit] if items else []}
 
         self.build_response(response)
